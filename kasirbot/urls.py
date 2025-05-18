0a7f4e2be75d851a1bf0d86651845452c6599d70
Revenage97 <angga200997@gmail.com>
2025-05-18 13:02:39 +0700

    Update urls.py

--- kasirbot/urls.py
+++ kasirbot/urls.py
@@ -1,29 +1,18 @@
-# kasirbot/urls.py
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    # Admin
     path('admin/', admin.site.urls),
-
-    # Halaman root menampilkan LoginView
     path(
         '',
         LoginView.as_view(template_name='registration/login.html'),
         name='login'
     ),
-
-    # Logout via POST (dengan form di base.html)
     path(
         'logout/',
-        LogoutView.as_view(),
+        LogoutView.as_view(next_page='/'),
         name='logout'
     ),
-
-    # Setelah login, semua dashboard di bawah /dashboard/
-    path(
-        'dashboard/',
-        include('inventory.urls')
-    ),
+    path('dashboard/', include('inventory.urls')),
 ]
